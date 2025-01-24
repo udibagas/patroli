42325d5ae869b27bf1253115146269298844d945
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-01-24 19:17:24 +0700

    seed sites

---
+++ seeders/20240831113921-seed-sites.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("Sites", [
+      {
+        code: "01",
+        name: "Head Office",
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Sites");
+  },
+};
